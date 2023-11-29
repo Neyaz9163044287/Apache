082c685f0149b533ca3407d482cf99f6dfce8bb1
Neyaz9163044287 <mdneyaz.ahmed08@gmail.com>
2023-11-29 15:59:17 +0530

    ScriptUpdate

--- src/test/java/Astor/ScriptTest.java
+++ src/test/java/Astor/ScriptTest.java
@@ -1,12 +1,19 @@
 package Astor;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ScriptTest 
 {
 	@Test
-	public void add()
+	public void add() throws InterruptedException
 	{
-		System.out.println("------Hello-----");
+		System.out.println("************Hello************");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		String title = driver.getTitle();
+		System.out.println("**********"+title+"**********");
+		Thread.sleep(1000);
 	}
 }
