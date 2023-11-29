de75f00587649c31329ba18872240bed28d3b338
Neyaz9163044287 <mdneyaz.ahmed08@gmail.com>
2023-11-29 13:52:00 +0530

    Sample1

---
+++ src/test/java/Astor/Sample1.java
@@ -0,0 +1,18 @@
+package Astor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample1 {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		String title = driver.getTitle();
+		System.out.println(title);
+		Thread.sleep(3000);
+
+	}
+
+}
